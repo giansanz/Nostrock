d5607e40b8a35101273a94879cc95f5a6e15e2bb
giansanz <60559787+giansanz@users.noreply.github.com>
2020-02-04 09:44:57 -0800

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,4 @@
+from stock_price import stock_watch
 from textblob import TextBlob
 from bs4 import BeautifulSoup
 import requests
@@ -16,11 +17,19 @@ class Analysis:
 		soup = BeautifulSoup(response.text, 'html.parser')
 		headline_results = soup.find_all('div', class_='kCrYT')
 		list_sites = []
-		for links in headline_results:
-			link = links.find('a')['href']
-			site = link.strip('/url?q=')
-			head, sep, tail = site.partition('&')
-			list_sites.append(head)
+		try:
+			for links in headline_results:
+				website = links.find('a')['href']
+				site = website.strip('/url?q=')
+				head, sep, tail = site.partition('&')
+				list_sites.append(head)
+		except:
+			pass
+			
+
+
+		list_sites = list(dict.fromkeys(list_sites))
+
 
 		for pages in range(2):
 			response = requests.get(list_sites[pages])
@@ -31,9 +40,11 @@ class Analysis:
 				self.sentiment += blob.sentiment.polarity / len(info)
 				self.subjectivity += blob.sentiment.subjectivity / len(info)
 				
-
-
-						   
-a = Analysis(argv[1])
-print a.term, 'Subjectivity: ', a.subjectivity, 'Sentiment: ', a.sentiment
-
+				
+try:
+	a = Analysis(argv[1])
+	a.pages()				
+	print('\n\t\tCurrent stock price: ', stock_watch(argv[1]))
+	print('\n\t', a.term, 'Subjectivity: ', a.subjectivity, 'Sentiment: ', a.sentiment)
+except:
+	print(a.term, ' is not a stock... Here is the Subjectivity and Sentiment of the ', a.term, 'search.\n', 'Subjectivity: ', a.subjectivity, 'Sentiment: ', a.sentiment )
