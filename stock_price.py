5baa9c8c263bf350a87f05e19e19c0b71305fe53
giansanz <60559787+giansanz@users.noreply.github.com>
2020-02-04 09:32:58 -0800

    Add files via upload

---
+++ stock_price.py
@@ -0,0 +1,12 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+
+def stock_watch(stock):
+    source = requests.get('http://money.cnn.com/quote/quote.html?symb=' + stock)
+    soup = BeautifulSoup(source.text, 'lxml')
+    container = soup.find('td', class_='wsod_last')
+    return container.find("span", {'streamformat':'ToHundredth'}).text
+
+
+
