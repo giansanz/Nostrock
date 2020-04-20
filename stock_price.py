1a158f2a64fc23c054d8e1450bf0d4950d48735d
giansanz <60559787+giansanz@users.noreply.github.com>
2020-04-20 07:01:44 -0400

    Update stock_price.py

--- stock_price.py
+++ stock_price.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 def stock_watch(stock):
     source = requests.get('http://money.cnn.com/quote/quote.html?symb=' + stock)
-    soup = BeautifulSoup(source.text, 'lxml')
+    soup = BeautifulSoup(source.text, 'html.parser')
     container = soup.find('td', class_='wsod_last')
     return container.find("span", {'streamformat':'ToHundredth'}).text
 
